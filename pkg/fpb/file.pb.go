6719756929549e3dd464a759c74f117d38beac27
daffadon <daffaputranarendra9@gmail.com>
2025-06-04 14:20:43 +0700

    feat(proto): add RemoveProfileImage to the contract

--- pkg/fpb/file.pb.go
+++ pkg/fpb/file.pb.go
@@ -109,6 +109,50 @@ func (x *ImageName) GetName() string {
 	return ""
 }
 
+type Status struct {
+	state         protoimpl.MessageState `protogen:"open.v1"`
+	Status        bool                   `protobuf:"varint,1,opt,name=status,proto3" json:"status,omitempty"`
+	unknownFields protoimpl.UnknownFields
+	sizeCache     protoimpl.SizeCache
+}
+
+func (x *Status) Reset() {
+	*x = Status{}
+	mi := &file_file_proto_msgTypes[2]
+	ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+	ms.StoreMessageInfo(mi)
+}
+
+func (x *Status) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*Status) ProtoMessage() {}
+
+func (x *Status) ProtoReflect() protoreflect.Message {
+	mi := &file_file_proto_msgTypes[2]
+	if x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use Status.ProtoReflect.Descriptor instead.
+func (*Status) Descriptor() ([]byte, []int) {
+	return file_file_proto_rawDescGZIP(), []int{2}
+}
+
+func (x *Status) GetStatus() bool {
+	if x != nil {
+		return x.Status
+	}
+	return false
+}
+
 var File_file_proto protoreflect.FileDescriptor
 
 const file_file_proto_rawDesc = "" +
@@ -118,10 +162,13 @@ const file_file_proto_rawDesc = "" +
 	"\x05Image\x12\x14\n" +
 	"\x05image\x18\x01 \x01(\fR\x05image\"\x1f\n" +
 	"\tImageName\x12\x12\n" +
-	"\x04name\x18\x01 \x01(\tR\x04name2?\n" +
+	"\x04name\x18\x01 \x01(\tR\x04name\" \n" +
+	"\x06Status\x12\x16\n" +
+	"\x06status\x18\x01 \x01(\bR\x06status2t\n" +
 	"\vFileService\x120\n" +
 	"\x10SaveProfileImage\x12\n" +
-	".fpb.Image\x1a\x0e.fpb.ImageName\"\x00B(Z&github.com/dropboks/proto-file/pkg/fpbb\x06proto3"
+	".fpb.Image\x1a\x0e.fpb.ImageName\"\x00\x123\n" +
+	"\x12RemoveProfileImage\x12\x0e.fpb.ImageName\x1a\v.fpb.Status\"\x00B(Z&github.com/dropboks/proto-file/pkg/fpbb\x06proto3"
 
 var (
 	file_file_proto_rawDescOnce sync.Once
@@ -135,16 +182,19 @@ func file_file_proto_rawDescGZIP() []byte {
 	return file_file_proto_rawDescData
 }
 
-var file_file_proto_msgTypes = make([]protoimpl.MessageInfo, 2)
+var file_file_proto_msgTypes = make([]protoimpl.MessageInfo, 3)
 var file_file_proto_goTypes = []any{
 	(*Image)(nil),     // 0: fpb.Image
 	(*ImageName)(nil), // 1: fpb.ImageName
+	(*Status)(nil),    // 2: fpb.Status
 }
 var file_file_proto_depIdxs = []int32{
 	0, // 0: fpb.FileService.SaveProfileImage:input_type -> fpb.Image
-	1, // 1: fpb.FileService.SaveProfileImage:output_type -> fpb.ImageName
-	1, // [1:2] is the sub-list for method output_type
-	0, // [0:1] is the sub-list for method input_type
+	1, // 1: fpb.FileService.RemoveProfileImage:input_type -> fpb.ImageName
+	1, // 2: fpb.FileService.SaveProfileImage:output_type -> fpb.ImageName
+	2, // 3: fpb.FileService.RemoveProfileImage:output_type -> fpb.Status
+	2, // [2:4] is the sub-list for method output_type
+	0, // [0:2] is the sub-list for method input_type
 	0, // [0:0] is the sub-list for extension type_name
 	0, // [0:0] is the sub-list for extension extendee
 	0, // [0:0] is the sub-list for field type_name
@@ -161,7 +211,7 @@ func file_file_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: unsafe.Slice(unsafe.StringData(file_file_proto_rawDesc), len(file_file_proto_rawDesc)),
 			NumEnums:      0,
-			NumMessages:   2,
+			NumMessages:   3,
 			NumExtensions: 0,
 			NumServices:   1,
 		},
--- pkg/fpb/file_grpc.pb.go
+++ pkg/fpb/file_grpc.pb.go
@@ -19,7 +19,8 @@ import (
 const _ = grpc.SupportPackageIsVersion9
 
 const (
-	FileService_SaveProfileImage_FullMethodName = "/fpb.FileService/SaveProfileImage"
+	FileService_SaveProfileImage_FullMethodName   = "/fpb.FileService/SaveProfileImage"
+	FileService_RemoveProfileImage_FullMethodName = "/fpb.FileService/RemoveProfileImage"
 )
 
 // FileServiceClient is the client API for FileService service.
@@ -27,6 +28,7 @@ const (
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
 type FileServiceClient interface {
 	SaveProfileImage(ctx context.Context, in *Image, opts ...grpc.CallOption) (*ImageName, error)
+	RemoveProfileImage(ctx context.Context, in *ImageName, opts ...grpc.CallOption) (*Status, error)
 }
 
 type fileServiceClient struct {
@@ -47,11 +49,22 @@ func (c *fileServiceClient) SaveProfileImage(ctx context.Context, in *Image, opt
 	return out, nil
 }
 
+func (c *fileServiceClient) RemoveProfileImage(ctx context.Context, in *ImageName, opts ...grpc.CallOption) (*Status, error) {
+	cOpts := append([]grpc.CallOption{grpc.StaticMethod()}, opts...)
+	out := new(Status)
+	err := c.cc.Invoke(ctx, FileService_RemoveProfileImage_FullMethodName, in, out, cOpts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // FileServiceServer is the server API for FileService service.
 // All implementations must embed UnimplementedFileServiceServer
 // for forward compatibility.
 type FileServiceServer interface {
 	SaveProfileImage(context.Context, *Image) (*ImageName, error)
+	RemoveProfileImage(context.Context, *ImageName) (*Status, error)
 	mustEmbedUnimplementedFileServiceServer()
 }
 
@@ -64,6 +77,9 @@ type UnimplementedFileServiceServer struct{}
 
 func (UnimplementedFileServiceServer) SaveProfileImage(context.Context, *Image) (*ImageName, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method SaveProfileImage not implemented")
+}
+func (UnimplementedFileServiceServer) RemoveProfileImage(context.Context, *ImageName) (*Status, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method RemoveProfileImage not implemented")
 }
 func (UnimplementedFileServiceServer) mustEmbedUnimplementedFileServiceServer() {}
 func (UnimplementedFileServiceServer) testEmbeddedByValue()                     {}
@@ -104,6 +120,24 @@ func _FileService_SaveProfileImage_Handler(srv interface{}, ctx context.Context,
 	return interceptor(ctx, in, info, handler)
 }
 
+func _FileService_RemoveProfileImage_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(ImageName)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(FileServiceServer).RemoveProfileImage(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: FileService_RemoveProfileImage_FullMethodName,
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(FileServiceServer).RemoveProfileImage(ctx, req.(*ImageName))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 // FileService_ServiceDesc is the grpc.ServiceDesc for FileService service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
@@ -114,6 +148,10 @@ var FileService_ServiceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "SaveProfileImage",
 			Handler:    _FileService_SaveProfileImage_Handler,
+		},
+		{
+			MethodName: "RemoveProfileImage",
+			Handler:    _FileService_RemoveProfileImage_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
